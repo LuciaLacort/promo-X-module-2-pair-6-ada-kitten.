9b11a9308cfaa097288b809068ecd23076b35681
LuciaLacort <lacortlucia@gmail.com>
2024-04-08 11:27:44 +0200

    Condicionales

--- main.js
+++ main.js
@@ -70,3 +70,47 @@ buttonCancel.addEventListener('click', (event)=>{
   addKittenSection.classList.add('collapsed');
   newForm.reset();
 });
+
+const input_search_desc = document.querySelector('.js_in_search_desc');
+
+/*const kittenDesc1 = document.querySelector('.card_description')
+console.log(kittenDesc1);
+
+const kittenDesc2 = document.querySelector('.card_description')
+console.log(kittenDesc2);
+
+const kittenDesc3 = document.querySelector('.card_description')
+console.log(kittenDesc3);*/
+
+
+const searchBtn = document.querySelector('.js_search_btn');
+
+searchBtn.addEventListener('click', (ev)=>{
+  const descrSearchText = input_search_desc.value;
+  console.log(descrSearchText);
+
+  if(kittenOne.includes(descrSearchText) ) {
+    kittenOne.classList.remove('collapsed');
+  } else{
+      kittenOne.classList.add('collapsed');
+    }
+
+  if( kittenTwo.includes(descrSearchText) ) {
+    kittenTwo.classList.remove('collapsed');
+  } else{
+      kittenTwo.classList.add('collapsed');
+    }
+  if( kittenThree.includes(descrSearchText) ) {
+    kittenThree.classList.remove('collapsed');
+  } else{
+    kittenThree.classList.add('collapsed');
+  }
+  
+});
+
+
+
+
+
+
+
