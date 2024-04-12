904fa1bb77aa14c95a6657e1356d158e4e913647
LuciaLacort <lacortlucia@gmail.com>
2024-04-12 11:27:51 +0200

    objetos

--- main.js
+++ main.js
@@ -2,64 +2,70 @@
 
 const kittenList = document.querySelector('.js-list');
 
-const kittenOneImage = 'https://dev.adalab.es/gato-siames.webp';
-const kittenOneName = 'Anastacio';
-const kittenOneNameUpper = kittenOneName.toUpperCase();
-const kittenOneDesc =
-  'Porte elegante, su patrón de color tan característico y sus ojos de un azul intenso, pero su historia se remonta a Asía al menos hace 500 años, donde tuvo su origen muy posiblemente.';
-const kittenOneRace = '';
+const kittenData_1 = {
+  image: 'https://dev.adalab.es/gato-siames.webp',
+  name:'Anastacio',
+  desc:  'Porte elegante, su patrón de color tan característico y sus ojos de un azul intenso, pero su historia se remonta a Asía al menos hace 500 años, donde tuvo su origen muy posiblemente.',
+  race: '',
+}
+const kittenData_2 = {
+  image: 'https://dev.adalab.es/sphynx-gato.webp',
+  name:'Fiona',
+  desc: 'Produce fascinación y curiosidad. Exótico, raro, bello, extraño hasta con pinta de alienígena han llegado a definir a esta raza gatuna que se caracteriza por la «ausencia» de pelo.',
+  race: '',
+}
+
+const kittenData_3 = {
+  image: 'https://dev.adalab.es/maine-coon-cat.webp',
+  name:'Cielo',
+  desc: 'Tienen la cabeza cuadrada y los ojos simétricos, por lo que su bella mirada se ha convertido en una de sus señas de identidad. Sus ojos son grandes y las orejas resultan largas y en punta.',
+  race:'maine coon',
+}
+
 
 const kittenOne = `<li class="card">
 <article>
   <img
     class="card_img"
-    src="${kittenOneImage}"
+    src="${kittenData_1.image}"
     alt="siames-cat"
   />
-  <h3 class="card_title">${kittenOneNameUpper}</h3>
-  <h4 class="card_race">${kittenOneRace}</h4>
-  <p class="card_description">${kittenOneDesc}</p>
+  <h3 class="card_title">${kittenData_1.name}</h3>
+  <h4 class="card_race">${kittenData_1.race}</h4>
+  <p class="card_description">${kittenData_1.desc}</p>
 </article>
 </li>`; 
 
-const kittenTwoImage = 'https://dev.adalab.es/sphynx-gato.webp';
-const kittenTwoName = 'Fiona';
-const kittenTwoNameUpper = kittenTwoName.toUpperCase();
-const kittenTwoDesc =
-  'Produce fascinación y curiosidad. Exótico, raro, bello, extraño hasta con pinta de alienígena han llegado a definir a esta raza gatuna que se caracteriza por la «ausencia» de pelo.';
-const kittenTwoRace = 'Sphynx';
 
 const kittenTwo = `<li class="card">
-<img
-  class="card_img"
-  src="${kittenTwoImage}"
-  alt="sphynx-cat"
-/>
-<h3 class="card_title">${kittenTwoNameUpper}</h3>
-<h4 class="card_race2">${kittenTwoRace}</h4>
-<p class="card_description">${kittenTwoDesc}</p>
+<article>
+  <img
+    class="card_img"
+    src="${kittenData_2.image}"
+    alt="siames-cat"
+  />
+  <h3 class="card_title">${kittenData_2.name}</h3>
+  <h4 class="card_race">${kittenData_2.race}</h4>
+  <p class="card_description">${kittenData_2.desc}</p>
+</article>
 </li>`;
 
-const kittenThreeImage = 'https://dev.adalab.es/maine-coon-cat.webp';
-const kittenThreeName = 'Cielo';
-const kittenThreeNameUpper = kittenThreeName.toUpperCase();
-console.log(kittenThreeNameUpper);
-const kittenThreeDesc =
-  'Tienen la cabeza cuadrada y los ojos simétricos, por lo que su bella mirada se ha convertido en una de sus señas de identidad. Sus ojos son grandes y las orejas resultan largas y en punta.';
-const kittenThreeRace = 'Maine Coon';
 
-const kittenThree = ` <li class="card">
-<img
-  class="card_img"
-  src="${kittenThreeImage}"
-  alt="maine-coon-cat"
-/>
-<h3 class="card_title">${kittenThreeNameUpper}</h3>
-<h4 class="card_race3">${kittenThreeRace}</h4>
-<p class="card_description">${kittenThreeDesc}</p>
+const kittenThree = `<li class="card">
+<article>
+  <img
+    class="card_img"
+    src="${kittenData_3.image}"
+    alt="siames-cat"
+  />
+  <h3 class="card_title">${kittenData_3.name}</h3>
+  <h4 class="card_race">${kittenData_3.race}</h4>
+  <p class="card_description">${kittenData_3.desc}</p>
+</article>
 </li>`;
 
 kittenList.innerHTML = kittenOne + kittenTwo + kittenThree;
+
 
 
 //Definir query selectors
@@ -78,11 +84,6 @@ function hideNewCatForm() {
   addKittenSection.classList.add('collapsed');
 }
 
-/*showFormButton.addEventListener("click", (event)=>{
-  addKittenSection.classList.toggle('collapsed');
-  console.log(event.currentTarget);
-  console.log(event.target);
-});*/
 
 //Crear la función que vamos a pasar de parámetro en el event listener con el código que se va a ejecutar al hacer click 
 
@@ -122,18 +123,25 @@ const inputDesc = document.querySelector('.js-input-desc');
 
 //Entonces tenemos que hacer una función que cree una card nueva con los parámetros que queremos que tenga.
 
-function renderKitten(url, name, raza, desc){
+const kittenData = {
+  image: '',
+  name: '',
+  desc: '',
+  race: '',
+};
+
+function renderKitten(kittenData){
 
   const newCat = `<li class="card">
   <article>
     <img
       class="card_img"
-      src=${url}
+      src=${kittenData.image}
       alt="cat"
     />
-    <h3 class="card_title">${name}</h3>
-    <h4 class="card_race">${raza}</h4>
-    <p class="card_description">${desc}</p>
+    <h3 class="card_title">${kittenData.name}</h3>
+    <h4 class="card_race">${kittenData.race}</h4>
+    <p class="card_description">${kittenData.desc}</p>
   </article>
   </li>`; 
   return newCat;
@@ -142,7 +150,23 @@ function renderKitten(url, name, raza, desc){
 //Y después creamos una función que recoja el value de cada input que hemos llamado con las variables anteriores, cada value en una variable una variable. Y le decimos que lo sume a la lista de gatos que tenemos invoando a la función que crea la tarjeta, pasando esas variables como argumentos
 
 
-function addNewKitten(event) {
+
+const handleNewKitten = (event) => {
+  const valueInput = event.target.value;
+  const idInput = event.target.id;
+  kittenData[idInput] = valueInput;
+  kittenList.innerHTML += renderKitten(kittenData);
+}
+
+
+
+kittenList.addEventListener('input', handleNewKitten);
+
+
+
+
+
+/*function addNewKitten(event) {
   
 
   const url = inputImage.value;
@@ -152,8 +176,7 @@ function addNewKitten(event) {
 
   // Estábamos pasando la función sin pasarle los argumentos
   kittenList.innerHTML += renderKitten(url, name, raza, desc);
-}
-
+}*/
 
 
 const alertEmptyFields = ()=>{
@@ -165,7 +188,7 @@ const alertEmptyFields = ()=>{
     alert.textContent = 'Por favor, complete ambos campos.';
   } else {
     alert.textContent = '';
-    addNewKitten()
+    handleNewKitten()
     hideNewCatForm();;
   }
 }
