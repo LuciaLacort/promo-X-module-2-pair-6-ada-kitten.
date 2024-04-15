4f7eb0b0de5dc8132859508628068b762ec22ec9
marina-fdz <marina.fdz.caceres@gmail.comgit config --global core.editor code>
2024-04-15 18:07:02 +0200

    add objects to objects branch

--- main.js
+++ main.js
@@ -1,6 +1,19 @@
 'use strict';
 
 const kittenList = document.querySelector('.js-list');
+const addKittenSection = document.querySelector('.js-new-kitten');
+const newForm = document.querySelector('.js-new-form');
+const showFormButton = document.querySelector('.js-kitten-add');
+const addKittenButton = document.querySelector('.js-btn-add');
+const inputImage = document.querySelector('.js-input-image');
+const inputName = document.querySelector('.js-input-name');
+const inputRace = document.querySelector('.js-input-raza');
+const inputDesc = document.querySelector('.js-input-desc');
+const buttonCancel = document.querySelector('.js-button-cancel');
+const input_search_desc = document.querySelector('.js_in_search_desc');
+const input_search_race = document.querySelector('.js_in_search_race');
+const searchBtn = document.querySelector('.js_search_btn');
+
 
 const kittenData_1 = {
   image: 'https://dev.adalab.es/gato-siames.webp',
@@ -19,63 +32,48 @@ const kittenData_3 = {
   image: 'https://dev.adalab.es/maine-coon-cat.webp',
   name:'Cielo',
   desc: 'Tienen la cabeza cuadrada y los ojos simétricos, por lo que su bella mirada se ha convertido en una de sus señas de identidad. Sus ojos son grandes y las orejas resultan largas y en punta.',
-  race:'maine coon',
+  race:'Maine Coon',
+}
+
+// Raza
+
+const noRace = (kitten) =>{
+
+  
+  if (kitten.race === '') {
+    kitten.race = `Uy que despiste, no sabemos su raza`;
+  } 
+  
+  };
+  
+  noRace(kittenData_1);
+  noRace(kittenData_2);
+  noRace(kittenData_3);
+
+
+function renderKitten(kittenData){
+
+  return `<li class="card">
+  <article>
+    <img
+      class="card_img"
+      src=${kittenData.image}
+      alt="cat"
+    />
+    <h3 class="card_title">${kittenData.name.toUpperCase()}</h3>
+    <h4 class="card_race js-race">${kittenData.race}</h4>
+    <p class="card_description">${kittenData.desc}</p>
+  </article>
+  </li>`; 
+  
 }
 
 
-
-const kittenOne = `<li class="card">
-<article>
-  <img
-    class="card_img"
-    src="${kittenData_1.image}"
-    alt="siames-cat"
-  />
-  <h3 class="card_title">${kittenData_1.name.toUpperCase()}</h3>
-  <h4 class="card_race js-race1">${kittenData_1.race.toUpperCase()}</h4>
-  <p class="card_description">${kittenData_1.desc}</p>
-</article>
-</li>`; 
-
-
-const kittenTwo = `<li class="card">
-<article>
-  <img
-    class="card_img"
-    src="${kittenData_2.image}"
-    alt="siames-cat"
-  />
-  <h3 class="card_title">${kittenData_2.name.toUpperCase()}</h3>
-  <h4 class="card_race js-race2">${kittenData_2.race.toUpperCase()}</h4>
-  <p class="card_description">${kittenData_2.desc}</p>
-</article>
-</li>`;
-
-
-const kittenThree = `<li class="card">
-<article>
-  <img
-    class="card_img"
-    src="${kittenData_3.image}"
-    alt="siames-cat"
-  />
-  <h3 class="card_title">${kittenData_3.name.toUpperCase()}</h3>
-  <h4 class="card_race js-race3">${kittenData_3.race.toUpperCase()}</h4>
-  <p class="card_description">${kittenData_3.desc}</p>
-</article>
-</li>`;
-
-kittenList.innerHTML = kittenOne + kittenTwo + kittenThree;
+kittenList.innerHTML = renderKitten(kittenData_1) + renderKitten(kittenData_2) + renderKitten(kittenData_3);
 
 
 
-//Definir query selectors
-
-const addKittenSection = document.querySelector('.js-new-kitten');
-const newForm = document.querySelector('.js-new-form');
-const showFormButton = document.querySelector('.js-kitten-add');
-const addKittenButton = document.querySelector('.js-btn-add');
-
+//Show/Hide New Kitten form
 
 
 function showNewCatForm() {
@@ -86,8 +84,6 @@ function hideNewCatForm() {
 }
 
 
-//Crear la función que vamos a pasar de parámetro en el event listener con el código que se va a ejecutar al hacer click 
-
 function handleClickNewCatForm(event) {
   event.preventDefault();
   if (addKittenSection.classList.contains('collapsed')) {
@@ -97,12 +93,10 @@ function handleClickNewCatForm(event) {
   }
 }
 
-//Escuchar el evento sobre el botón 
 
 showFormButton.addEventListener('click', handleClickNewCatForm);
 
-
-const buttonCancel = document.querySelector('.js-button-cancel');
+// Cancel Button
 
 buttonCancel.addEventListener('click', (event)=>{
   addKittenSection.classList.add('collapsed');
@@ -110,19 +104,7 @@ buttonCancel.addEventListener('click', (event)=>{
 });
 
 
-
-//declarar variables con los input de desc, name, race, url
-
-
-
-const inputImage = document.querySelector('.js-input-image');
-const inputName = document.querySelector('.js-input-name');
-const inputRace = document.querySelector('.js-input-raza');
-const inputDesc = document.querySelector('.js-input-desc');
-
-
-
-//Entonces tenemos que hacer una función que cree una card nueva con los parámetros que queremos que tenga.
+//New kitten
 
 const kittenData = {
   image: '',
@@ -130,29 +112,6 @@ const kittenData = {
   desc: '',
   race: '',
 };
-
-
-
-
-function renderKitten(kittenData){
-
-  const newCat = `<li class="card">
-  <article>
-    <img
-      class="card_img"
-      src=${kittenData.image}
-      alt="cat"
-    />
-    <h3 class="card_title">${kittenData.name}</h3>
-    <h4 class="card_race js-race">${kittenData.race}</h4>
-    <p class="card_description">${kittenData.desc}</p>
-  </article>
-  </li>`; 
-  return newCat;
-}
-
-//Y después creamos una función que recoja el value de cada input que hemos llamado con las variables anteriores, cada value en una variable una variable. Y le decimos que lo sume a la lista de gatos que tenemos invoando a la función que crea la tarjeta, pasando esas variables como argumentos
-
 
 
 const handleNewKitten = (event) => {
@@ -163,24 +122,7 @@ const handleNewKitten = (event) => {
   console.log(event.target.value);
 }
 
-
 newForm.addEventListener('input', handleNewKitten);
-
-
-
-
-
-/*function addNewKitten(event) {
-  
-
-  const url = inputImage.value;
-  const name = inputName.value;
-  const raza = inputRace.value;
-  const desc = inputDesc.value;
-
-  // Estábamos pasando la función sin pasarle los argumentos
-  kittenList.innerHTML += renderKitten(url, name, raza, desc);
-}*/
 
 
 const alertEmptyFields = ()=>{
@@ -193,12 +135,10 @@ const alertEmptyFields = ()=>{
   } else {
     alert.textContent = '';
     kittenList.innerHTML += renderKitten(kittenData);
-    // handleNewKitten();
     hideNewCatForm();
   }
 }
 
-// Se escucha al boton de añadir que al hacer click ejecuta la función que añade un gato nuevo, que a su vez recoge los valores de los inputs del formulario e invoca a la función que crea la tarjeta 
 
 const handleClickaddNewKitten = (event)=>{
   event.preventDefault();
@@ -210,100 +150,45 @@ addKittenButton.addEventListener('click', handleClickaddNewKitten);
 
 
 
-// raza ya FUNCIONA
-
-const noRace = () =>{
-
-const kittenRace1 = document.querySelector(".js-race1");
-if (kittenData_1.race === "") {
-  kittenRace1.innerHTML = `Uy que despiste, no sabemos su raza`;
-} 
-
-const kittenRace2 = document.querySelector(".js-race2");
-if (kittenData_2.race === "") {
-  kittenRace2.innerHTML = `Uy que despiste, no sabemos su raza`;
-} 
-
-const kittenRace3 = document.querySelector(".js-race3");
-if (kittenData_3.race === "") {
-  kittenRace3.innerHTML = `Uy que despiste, no sabemos su raza`;
-} 
-
-};
-
-noRace();
-
-
-
-
-
 //Buscar por descripción
 
 
-
-const input_search_desc = document.querySelector('.js_in_search_desc');
-const input_search_race = document.querySelector('.js_in_search_race');
-const searchBtn = document.querySelector('.js_search_btn');
-
 const filterKitten = () => {
   const descSearchText = input_search_desc.value;
-  const descSearchTextUpper = descSearchText.toUpperCase();
-
-  const kittenOneUpper = kittenOne.toUpperCase();
-  const kittenTwoUpper = kittenTwo.toUpperCase();
-  const kittenThreeUpper = kittenThree.toUpperCase();
-
-  kittenList.innerHTML = " ";
-
-  if(kittenOneUpper.includes(descSearchTextUpper)) {
-    kittenList.innerHTML += kittenOne;
-  } 
-  if(kittenTwoUpper.includes(descSearchTextUpper)) {
-    kittenList.innerHTML += kittenTwo;
-  }
-  if(kittenThreeUpper.includes(descSearchTextUpper)) {
-    kittenList.innerHTML += kittenThree;
-  } 
-  if(kittenOneUpper.includes(descSearchTextUpper) === false && kittenTwoUpper.includes(descSearchTextUpper) === false && kittenThreeUpper.includes(descSearchTextUpper) === false){
-    kittenList.innerHTML ='<p class="noKitten">No hay ningún gatito con esa característica.<p>';
-  }
-
-  
-};
-
-
-// Búsqueda por raza FUNCIONA SÓLO UNA BÚSQUEDA A LA VEZ
-
-const filterRaceKitten = (event) => {
+  let descSearchTextUpper = descSearchText.toUpperCase();
   const raceSearchText = input_search_race.value;
   const raceSearchTextUpper = raceSearchText.toUpperCase();
-  
-  kittenList.innerHTML = " ";
 
-  if(kittenData_1.race.toUpperCase() === raceSearchTextUpper) {
-    kittenList.innerHTML += kittenOne;
-  } 
-  if(kittenData_2.race.toUpperCase() === raceSearchTextUpper) {
-    kittenList.innerHTML += kittenTwo;
+  const kittenOneDescUpper = kittenData_1.desc.toUpperCase();
+  const kittenTwoDescUpper = kittenData_2.desc.toUpperCase();
+  const kittenThreeDescUpper = kittenData_3.desc.toUpperCase();
+
+  kittenList.innerHTML = '';
+
+  if(descSearchTextUpper === ''){
+    descSearchTextUpper = undefined;
   }
-  if(kittenData_3.race.toUpperCase() === raceSearchTextUpper) {
-    kittenList.innerHTML += kittenThree;
-  } 
 
-  console.log(raceSearchTextUpper);
+  if((kittenOneDescUpper.includes(descSearchTextUpper)) || (kittenData_1.race.toUpperCase() === raceSearchTextUpper)) {
+    kittenList.innerHTML += renderKitten(kittenData_1);
+    console.log(kittenOneDescUpper);
+    console.log(descSearchTextUpper);
+  } 
+  if(kittenTwoDescUpper.includes(descSearchTextUpper) || kittenData_2.race.toUpperCase() === raceSearchTextUpper) {
+    kittenList.innerHTML += renderKitten(kittenData_2);
+  }
+  if(kittenThreeDescUpper.includes(descSearchTextUpper) || kittenData_3.race.toUpperCase() === raceSearchTextUpper) {
+    kittenList.innerHTML += renderKitten(kittenData_3);
+  } 
   
-  // if(kittenData_1.race.toUpperCase() !== raceSearchTextUpper && kittenData_2.race.toUpperCase !== raceSearchTextUpper && kittenData_3.race.toUpperCase !== raceSearchTextUpper){
-  // kittenList.innerHTML ='<p class="noKitten">No hay ningún gatito de esa raza.<p>';
-  // }
 };
+
 
 
 const handleClickSearch = (event)=>{
     event.preventDefault();
     filterKitten();
-    // filterRaceKitten();;
-    
-   /* alertEmptyFields();*/
+   
 };
 
 searchBtn.addEventListener('click', handleClickSearch);
