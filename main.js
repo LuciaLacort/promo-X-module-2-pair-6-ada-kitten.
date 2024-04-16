c2c795222a7d4914afefe954d7c03b35038aa8c7
LuciaLacort <lacortlucia@gmail.com>
2024-04-16 11:35:26 +0200

    filter

--- main.js
+++ main.js
@@ -15,6 +15,27 @@ const input_search_race = document.querySelector('.js_in_search_race');
 const searchBtn = document.querySelector('.js_search_btn');
 
 
+// const kittenDataList = [
+//   {
+//     image: 'https://dev.adalab.es/gato-siames.webp',
+//     name:'Anastacio',
+//     desc:  'Porte elegante, su patrón de color tan característico y sus ojos de un azul intenso, pero su historia se remonta a Asía al menos hace 500 años, donde tuvo su origen muy posiblemente.',
+//     race: '',
+//   },
+//   {
+//     image: 'https://dev.adalab.es/sphynx-gato.webp',
+//     name:'Fiona',
+//     desc: 'Produce fascinación y curiosidad. Exótico, raro, bello, extraño hasta con pinta de alienígena han llegado a definir a esta raza gatuna que se caracteriza por la «ausencia» de pelo.',
+//     race: '',
+//   },
+//   {
+//     image: 'https://dev.adalab.es/maine-coon-cat.webp',
+//     name:'Cielo',
+//     desc: 'Tienen la cabeza cuadrada y los ojos simétricos, por lo que su bella mirada se ha convertido en una de sus señas de identidad. Sus ojos son grandes y las orejas resultan largas y en punta.',
+//     race:'Maine Coon',
+//   }
+// ]
+
 const kittenData_1 = {
   image: 'https://dev.adalab.es/gato-siames.webp',
   name:'Anastacio',
@@ -34,6 +55,9 @@ const kittenData_3 = {
   desc: 'Tienen la cabeza cuadrada y los ojos simétricos, por lo que su bella mirada se ha convertido en una de sus señas de identidad. Sus ojos son grandes y las orejas resultan largas y en punta.',
   race:'Maine Coon',
 }
+
+const kittenDataList = [kittenData_1, kittenData_2, kittenData_3];
+console.log(kittenDataList[0]);
 
 // Raza
 
@@ -68,8 +92,17 @@ function renderKitten(kittenData){
   
 }
 
+function renderKittenList() {
+  kittenList.innerHTML = "";
+  for (const kitten of kittenDataList){
+    kittenList.innerHTML += renderKitten(kitten);
+  }
 
-kittenList.innerHTML = renderKitten(kittenData_1) + renderKitten(kittenData_2) + renderKitten(kittenData_3);
+}
+
+renderKittenList(kittenDataList);
+
+// kittenList.innerHTML = renderKitten(kittenData_1) + renderKitten(kittenData_2) + renderKitten(kittenData_3);
 
 
 
@@ -154,35 +187,46 @@ addKittenButton.addEventListener('click', handleClickaddNewKitten);
 
 
 const filterKitten = () => {
-  const descSearchText = input_search_desc.value;
-  let descSearchTextUpper = descSearchText.toUpperCase();
-  const raceSearchText = input_search_race.value;
-  const raceSearchTextUpper = raceSearchText.toUpperCase();
+  // const descSearchText = input_search_desc.value;
+  // let descSearchTextUpper = descSearchText.toUpperCase();
+  // const raceSearchText = input_search_race.value;
+  // const raceSearchTextUpper = raceSearchText.toUpperCase();
 
-  const kittenOneDescUpper = kittenData_1.desc.toUpperCase();
-  const kittenTwoDescUpper = kittenData_2.desc.toUpperCase();
-  const kittenThreeDescUpper = kittenData_3.desc.toUpperCase();
+  // const kittenOneDescUpper = kittenData_1.desc.toUpperCase();
+  // const kittenTwoDescUpper = kittenData_2.desc.toUpperCase();
+  // const kittenThreeDescUpper = kittenData_3.desc.toUpperCase();
 
   kittenList.innerHTML = '';
 
-  if(descSearchTextUpper === ''){
-    descSearchTextUpper = undefined;
+  for (const kitten of kittenDataList){
+    if (kitten.desc.includes(input_search_desc.value) || kitten.race.includes(input_search_race.value)){
+      kittenList.innerHTML += renderKitten(kitten);
+
+    }
+
+    // if ((kitten.race === input_search_race.value) || (kitten.desc.includes(input_search_desc.value))){
+    //   kittenList.innerHTML += renderKitten(kitten);
+    // }
   }
 
-  if((kittenOneDescUpper.includes(descSearchTextUpper)) || (kittenData_1.race.toUpperCase() === raceSearchTextUpper)) {
-    kittenList.innerHTML += renderKitten(kittenData_1);
-    console.log(kittenOneDescUpper);
-    console.log(descSearchTextUpper);
-  } 
-  if(kittenTwoDescUpper.includes(descSearchTextUpper) || kittenData_2.race.toUpperCase() === raceSearchTextUpper) {
-    kittenList.innerHTML += renderKitten(kittenData_2);
-  }
-  if(kittenThreeDescUpper.includes(descSearchTextUpper) || kittenData_3.race.toUpperCase() === raceSearchTextUpper) {
-    kittenList.innerHTML += renderKitten(kittenData_3);
-  } 
+
+  // if(descSearchTextUpper === ''){
+  //   descSearchTextUpper = undefined;
+  // }
+
+  // if((kittenOneDescUpper.includes(descSearchTextUpper)) || (kittenData_1.race.toUpperCase() === raceSearchTextUpper)) {
+  //   kittenList.innerHTML += renderKitten(kittenData_1);
+  //   console.log(kittenOneDescUpper);
+  //   console.log(descSearchTextUpper);
+  // } 
+  // if(kittenTwoDescUpper.includes(descSearchTextUpper) || kittenData_2.race.toUpperCase() === raceSearchTextUpper) {
+  //   kittenList.innerHTML += renderKitten(kittenData_2);
+  // }
+  // if(kittenThreeDescUpper.includes(descSearchTextUpper) || kittenData_3.race.toUpperCase() === raceSearchTextUpper) {
+  //   kittenList.innerHTML += renderKitten(kittenData_3);
+  // } 
   
 };
-
 
 
 const handleClickSearch = (event)=>{
